f05d1291b7e232e8342660c7e426b4bac4c5e364
Garryco Thandi <garrycoth1010@gmail.com>
2022-08-08 00:14:23 +1000

    Game loop added

--- FirstProject.java
+++ FirstProject.java
@@ -3,25 +3,37 @@ import java.util.Scanner;
 
     
 public class FirstProject{
+    
     public static void main(String[] args){
-        //Initializing the required variable
-        int topNum = 100;
-        int guessedNumber;
 
-        Random randomNumber = new Random(); //Create Random object
-        Scanner scan = new Scanner(System.in); //Create Scanner object
+        while (true){
+            //Initializing the required variable
+            int topNum = 100;
+            int guessedNumber;
 
-        int numberAnswer = randomNumber.nextInt(topNum); //The number that need to be guessed
-        System.out.print("Guess a number: ");
-        guessedNumber = scan.nextInt();
+            Random randomNumber = new Random(); //Create Random object
+            Scanner scan = new Scanner(System.in); //Create Scanner object
 
-        scan.close();
+            int numberAnswer = randomNumber.nextInt(topNum); //The number that need to be guessed
+            System.out.print("Guess a number (1-99): ");
+            guessedNumber = scan.nextInt();
 
-        if (guessedNumber == numberAnswer){
-            System.out.println("Wahoo");
+
+
+            if (guessedNumber == numberAnswer){
+                System.out.println("Wahoo");
+            }
+            else{
+                System.out.println("The correct answer is "+ numberAnswer);
+            }
+
+            System.out.println("Play again? (true/false)");    
+            boolean play = scan.nextBoolean();
+            if (play == false){
+                break;
+            }
+            
         }
-        else{
-            System.out.println("The correct answer is "+ numberAnswer);
-        }
+        
     }
 }
