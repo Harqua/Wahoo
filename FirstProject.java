29a59f46525d925ec47c8372edd9e916fcd9fa30
Garryco Thandi <garrycoth1010@gmail.com>
2022-08-07 23:53:55 +1000

    Finished the basic

--- FirstProject.java
+++ FirstProject.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+    
 public class FirstProject{
     public static void main(String[] args){
-        
+        //Initializing the required variable
+        int topNum = 100;
+        int guessedNumber;
+
+        Random randomNumber = new Random(); //Create Random object
+        Scanner scan = new Scanner(System.in); //Create Scanner object
+
+        int numberAnswer = randomNumber.nextInt(topNum); //The number that need to be guessed
+        System.out.print("Guess a number: ");
+        guessedNumber = scan.nextInt();
+
+        scan.close();
+
+        if (guessedNumber == numberAnswer){
+            System.out.println("Wahoo");
+        }
+        else{
+            System.out.println("The correct answer is "+ numberAnswer);
+        }
     }
 }
