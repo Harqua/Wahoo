62b2e7b31c43eb8d3bde60d011f9531ffe1f20e1
Garryco Thandi <garrycoth1010@gmail.com>
2022-08-08 21:35:17 +1000

    Added lives

--- FirstProject.java
+++ FirstProject.java
@@ -6,34 +6,53 @@ public class FirstProject{
     
     public static void main(String[] args){
 
-        while (true){
+        int lives = 3;
+        boolean play = true;
+
+        while (lives!=0 && play == true){
             //Initializing the required variable
-            int topNum = 100;
+            int topNum = 10;
             int guessedNumber;
 
             Random randomNumber = new Random(); //Create Random object
             Scanner scan = new Scanner(System.in); //Create Scanner object
 
             int numberAnswer = randomNumber.nextInt(topNum); //The number that need to be guessed
-            System.out.print("Guess a number (1-99): ");
+            
+            System.out.println("\nLives remaining: "+lives+"\n");
+            
+            System.out.print("Guess a number (0-9): ");
+            
+
             guessedNumber = scan.nextInt();
 
-
+            
 
             if (guessedNumber == numberAnswer){
-                System.out.println("Wahoo");
+                System.out.println("You guessed it!");
             }
             else{
                 System.out.println("The correct answer is "+ numberAnswer);
+                lives-=1; //penalty for losing
             }
-
-            System.out.println("Play again? (true/false)");    
-            boolean play = scan.nextBoolean();
-            if (play == false){
+            
+            if(lives>0){
+                System.out.print("Play again? (true/false) "); //asking whether players want to play again
                 
+                play = scan.nextBoolean();
+                
+                if (play == false){
+                    
+                    break;
+                }
+            }
+            else{
+
+                System.out.println("You lost the game.");
                 break;
             }
-            scan.close();
+            
+            // scan.close();
             
         }
         
